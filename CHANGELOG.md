48c7e8103e73a0066f6d369beed9bc730a1d15e6
chomosuke <a13323600@gmail.com>
2021-12-04 10:19:09 +1100

    ready to publish

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,3 @@
-## 0.0.1
+## 0.1.0
 
 Migrated code from my other project, added column and row, and wrote an examlpe app.
--- test/flex_with_main_child_test.dart
+++ test/flex_with_main_child_test.dart
@@ -3,5 +3,5 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:flex_with_main_child/src/flex_with_main_child.dart';
 
 void main() {
-  test('adds one to input values', () {});
+  // potentially test in the future
 }
